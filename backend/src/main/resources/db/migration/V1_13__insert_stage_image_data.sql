030f1341500efd8bfdb3cb12c05a0d4d2ce5f899
Saskia Kriibi <siim02@tlu.ee>
2023-12-05 19:25:23 +0200

    CR fixes 1

--- backend/src/main/resources/db/migration/V1_13__insert_stage_image_data.sql
+++ backend/src/main/resources/db/migration/V1_13__insert_stage_image_data.sql
@@ -22,11 +22,10 @@ DECLARE
     i INT;
 BEGIN
     FOR i IN 1..num_iterations LOOP
-            EXECUTE 'CALL image_insert($1, $2)' USING stage_name, base_file_name || '_' || i || '.jpg';
-        END LOOP;
+        EXECUTE 'CALL image_insert($1, $2)' USING stage_name, base_file_name || '_' || i || '.jpg';
+    END LOOP;
 END;
 $$;
-
 
 call image_insert('Jõhvi laululava', 'Jõhvi_1.jpg');
 call image_insert('Jõhvi laululava', 'Jõhvi_2.jpg');
@@ -42,7 +41,6 @@ call image_insert('Iisaku laululava', 'lisaku_1.jpg');
 call image_insert('Iisaku laululava', 'lisaku_2.jpg');
 
 
-CALL insert_images('Käru laululava', 'Käru', 9);
 CALL insert_images('Keava laululava', 'Kaeva', 6);
 CALL insert_images('Kohtla-Nõmme laululava', 'Kohtla-Nõmme', 3);
 CALL insert_images('Kulja laululava', 'Kulja', 3);
@@ -50,10 +48,11 @@ CALL insert_images('Kumma laululava', 'Kumma', 12);
 CALL insert_images('Käru laululava', 'Käru', 10);
 CALL insert_images('Lohusuu', 'Lohusuu', 4);
 CALL insert_images('Lüganuse laululava', 'Lüganuse', 5);
-CALL insert_images('Oonurme laululava', 'Oonurme', 4);
+!CALL insert_images('Oonurme laululava', 'Oonurme', 4);
 CALL insert_images('Saka mõisa suvelava', 'Saka_mois', 4);
 CALL insert_images('Ingliste kõlakoda', 'Ingliste', 14);
 CALL insert_images('Sonda kõlakoda', 'Sonda', 4);
 CALL insert_images('Toila laululava', 'Toila', 6);
 CALL insert_images('Vasknarva kõlakoda (ordulinnuse varemed)', 'Vasknarva', 2);
 CALL insert_images('Valtu-Nurme laululava', 'Valtu-Nurme', 14);
+
